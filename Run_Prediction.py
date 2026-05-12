189ba522f69af0184ff8291c779c8648f97c0358
dazege <zegeshidalao@163.com>
2026-05-12 13:47:42 +0800

    Update Run_Prediction.py

--- Run_Prediction.py
+++ Run_Prediction.py
@@ -14,7 +14,7 @@ import time
 
 
 def ProteinEmbedding(pbar,data):
-    model, alphabet = esm.pretrained.load_model_and_alphabet("esm2_t33_650M_UR50D.pt")
+    model, alphabet = esm.pretrained.esm2_t33_650M_UR50D()
     batch_converter = alphabet.get_batch_converter()
     model.eval()
     batch_labels, batch_strs, batch_tokens = batch_converter(data)
